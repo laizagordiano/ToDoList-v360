fb3b29e24a7a995ceb337afad26cac02117da6ee
laizagordiano <laizagordiano.5@gmail.com>
2026-01-17 23:49:00 -0300

    chore: set root to pages#home and tidy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
+  #Rota para página inicial
+  root to: "pages#home"
+
   #Rota para cadastro de usuários
   resources :users, only: [ :new, :create ] 
 
   #Rota para sessões de usuário (login/logout)
   resource :session, only: [ :new, :create, :destroy ] 
 
-
   resources :passwords, param: :token
   resources :lists do
     resources :tasks
   end
-
-  root to: "lists#index"
 
   get "confirm/:token", to: "confirmations#edit", as: :edit_confirmation
 
